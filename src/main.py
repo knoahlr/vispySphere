57e0c56888f173425434b1a6afb5f14d202fba13
unknown <noahlangat@cmail.carleton.ca>
2018-10-19 10:58:46 -0400

    minor changes'

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         os.makedirs(r"../logs")
         
     logFile = open(Path(r"../logs/mainLog.log"), 'w')
-    # sys.stdout = logFile
+    sys.stdout = logFile
 
     myappid = 'mycompany.myproduct.subproduct.version' # arbitrary string
     ''' https://stackoverflow.com/questions/1551605/how-to-set-applications-taskbar-icon-in-windows-7/1552105#1552105 '''
--- src/sceneWindow.py
+++ src/sceneWindow.py
@@ -2,6 +2,7 @@ import sys
 from vispy import scene, app
 from vispy.visuals.transforms import STTransform
 from vispy.visuals.filters import Clipper, Alpha, ColorFilter
+from vispy.util.quaternion import Quaternion
 
 from PyQt5 import QtGui, QtCore, QtWidgets
 from PyQt5.QtWidgets import QWidget, QFormLayout, QMainWindow, QGroupBox, QVBoxLayout, QHBoxLayout, \
@@ -11,6 +12,9 @@ from defaultMenuBar import DefaultMenuBar
 
 from pathlib import Path
 import numpy as np
+
+from PIL import Image
+import cv2
 
 
 ICON = Path(r'..\articles\atom.png')
@@ -39,7 +43,7 @@ class SceneWindow(QMainWindow):
 
         self.Icon = QtGui.QIcon(str(ICON))
         self.setMinimumSize(self.sizeHint())
-        self.resize(1200, 800)
+        self.resize(1600, 800)
         self.setWindowTitle('Vispy 3D')
         self.setWindowIcon(self.Icon)
     
@@ -60,24 +64,27 @@ class SceneWindow(QMainWindow):
 
         self.canvasWidget = QWidget()
         self.canvasWidget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.canvasWidgetLayout = QHBoxLayout()
+
+
+        ''' Rendered Video of scene'''
+        self.videoWidget = QtWidgets.QGraphicsView()
+        self.videoWidget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         
+
         '''Labels'''
 
         self.setFunctionLabel = QLabel("Set Function")
         self.setFunctionBox = QLineEdit() #placeholder="input Function in Cartesian Co-ordSinates"SSS
-
         self.controlFrameLayout.setWidget(0, QFormLayout.LabelRole, self.setFunctionLabel)
         self.controlFrameLayout.setWidget(0, QFormLayout.FieldRole, self.setFunctionBox)
 
 
         # self.canvasFrameLayout.addWidget(self.canvasWidget)
         self.addCanvas()
-
-
-
-
-
-
+        self.canvasWidget.setLayout(self.canvasWidgetLayout)
+        self.canvasFrameLayout.addWidget(self.canvasWidget)
+        
         self.verticalLayout.addWidget(self.canvasFrame)
         self.verticalLayout.addWidget(self.controlFrame)
 
@@ -89,20 +96,26 @@ class SceneWindow(QMainWindow):
         self.canvas = scene.SceneCanvas(keys='interactive', bgcolor='white',
                            size=(800, 600), show=False)
 
-        view = self.canvas.central_widget.add_view()
-        view.camera = 'arcball'
+        self.view = self.canvas.central_widget.add_view()
+    
+        self.view.camera = 'turntable'
 
-        self.sphere1 = scene.visuals.Sphere(radius=.3, method='latitude', parent=view.scene,
+        print(self.view, dir(self.view))
+        print(self.view.camera, dir(self.view.camera))
+        self.newCameraRotation = Quaternion()
+
+        self.sphere1 = scene.visuals.Sphere(radius=.3, method='latitude', parent=self.view.scene,
                                     edge_color='blue')
 
-        self.sphere2 = scene.visuals.Sphere(radius=.2, method='ico', parent=view.scene,
+        self.sphere2 = scene.visuals.Sphere(radius=.2, method='ico', parent=self.view.scene,
                                     edge_color='blue')
 
         self.sphere3 = scene.visuals.Sphere(radius=.3, rows=10, cols=10, depth=10,
-                                    method='cube', parent=view.scene,
+                                    method='cube', parent=self.view.scene,
                                     edge_color='blue')
+        self.axis = scene.visuals.XYZAxis(parent=self.view.scene)
 
-        self.plane = scene.visuals.Plane(edge_color='blue', parent=view.scene, width=10, height=10)
+        self.plane = scene.visuals.Plane(edge_color='blue', parent=self.view.scene, width=10, height=10)
         self.plane.transform = STTransform(translate=[-2.5, -2.5, -2.5])
 
 
@@ -111,12 +124,14 @@ class SceneWindow(QMainWindow):
 
         
 
-        view.camera.set_range(x=[-3, 3], y=[-3,3], z=[-3,3])
+        self.view.camera.set_range(x=[-3, 3], y=[-3,3], z=[-3,3])
+        self.view.camera.set_state({'center': [0.0, 0.0, 0.0]})
 
         self.canvas.create_native()
-        self.canvasWidget = self.canvas.native
 
-        self.canvasFrameLayout.addWidget(self.canvasWidget)
+        self.canvasWidgetLayout.addWidget(self.canvas.native)
+        self.canvasWidgetLayout.addWidget(self.videoWidget)
+
         self.sphereRanges = list(np.linspace(-2.5,2.5,100))
 
         self.transform()
@@ -131,14 +146,48 @@ class SceneWindow(QMainWindow):
         # timer = app.Timer(interval = 0.1, connect=self.update, start=True)
         # app.run()
 
+    def setImage(self):
+
+        # self.view.camera.set_range(x=[-5, 1], y=[-5,1], z=[-3,3])
+        # self.view.camera.set_state({'center': [0.0, 0.0, 0.0]})
+        
+        imageArray = self.canvas.render()
+
+        rgbImage = cv2.cvtColor(imageArray, cv2.COLOR_BGR2RGB)
+        
+        image = QtGui.QImage(rgbImage.data, imageArray.shape[1], imageArray.shape[0], QtGui.QImage.Format_RGB888)
+        
+
+        scene = QtWidgets.QGraphicsScene()
+        pixmapItem = QtWidgets.QGraphicsPixmapItem(QtGui.QPixmap.fromImage(image))
+        scene.addItem(pixmapItem)
+
+        self.videoWidget.setScene(scene)
 
            
 
     def update(self):
         
+
         cord = self.sphereRanges[self.rangeIndex]
         self.sphere1.transform = STTransform(translate=[-2.5, 0, cord])
         self.sphere3.transform = STTransform(translate=[cord, -2.5, 1])
+        # print(self.canvas.render())
+        print(self.view.camera.get_state(),self.view.camera.transform ,(self.view.camera._xlim, self.view.camera._ylim, self.view.camera._zlim,))
+
+        self.newCameraRotation = Quaternion(1, 0, cord, 3)
+        self.newCameraRotation =  self.newCameraRotation.create_from_euler_angles(0, 0, cord*36)
+
+        # self.canvas.push_viewport((3,3, cord*36,3))
+        
+        # self.view.camera.set_range( x=[-cord,cord],z=[-cord,cord])
+        # self.view.camera._quaternion = self.newCameraRotation
+        # self.view.camera._rotate_tr()
+        self.view.camera.elevation = 20
+        self.view.camera.azimuth = cord *72
+
+        
+        self.setImage()
         
         self.rangeIndex += 1
 
@@ -146,11 +195,4 @@ class SceneWindow(QMainWindow):
             self.sphereRanges = [elem*-1 for elem in self.sphereRanges]
             self.rangeIndex = 0
 
-        
 
-
-
-
-
-
-        
